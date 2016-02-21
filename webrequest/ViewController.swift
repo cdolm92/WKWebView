ef32925c6abc57be0c1409f188f23b879ce91b0f
cdolm92 <cdolm92@gmail.com>
2016-02-20 21:50:34 -0500

    first commit

---
+++ webrequest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webrequest
+//
+//  Created by Christella on 2/20/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

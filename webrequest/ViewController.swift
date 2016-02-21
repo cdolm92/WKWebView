e57a5141cf6fffc0a038116149d87aeb6bcbc79e
cdolm92 <cdolm92@gmail.com>
2016-02-20 22:11:01 -0500

    swift webview works

--- webrequest/ViewController.swift
+++ webrequest/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
-import WebKit 
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var container: UIView!
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        webView = WKWebView()
+        container.addSubview(webView)
         
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+        let urlString = "https://developer.apple.com/swift/"
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        webView.loadRequest(request)
+
     }
 
     override func didReceiveMemoryWarning() {
